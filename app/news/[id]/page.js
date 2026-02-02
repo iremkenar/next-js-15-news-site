bc0b7fc3912ec40e35bc9b58909ddd2de00b9e39
IRKENAR <irem.kenar@outlook.com>
2026-02-02 18:11:04 +0300

    Update styles

--- app/news/[id]/page.js
+++
@@ -1,9 +0,0 @@
-export default async function NewsDetailPage({ params }) {
-    const newsId = await params.id;
-    return (
-        <>
-            <h1>News Detail Page</h1>
-            <p>News ID: {newsId}</p>
-        </>
-    );
-}
