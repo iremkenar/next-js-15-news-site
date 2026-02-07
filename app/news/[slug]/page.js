35cfc16e53a7cc5cf79d29d99eab2dd463b169a9
IRKENAR <irem.kenar@outlook.com>
2026-02-07 15:52:50 +0300

    Added and interceptor route

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,11 +1,26 @@
-import { DUMMY_NEWS } from '@/dummy-news';
-import NewsList from '@/components/news-list';
+import { notFound } from 'next/navigation';
+import Link from 'next/link';
 
-export default function NewsPage() {
+import { DUMMY_NEWS } from '@/dummy-news';
+
+export default function NewsDetailPage({ params }) {
+    const newsSlug = params.slug;
+    const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
+
+    if (!newsItem) {
+        notFound();
+    }
+
     return (
-        <>
-            <h1>News Page</h1>
-            <NewsList news={DUMMY_NEWS} />
-        </>
+        <article className="news-article">
+            <header>
+                <Link href={`/news/${newsItem.slug}/image`}>
+                    <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                </Link>
+                <h1>{newsItem.title}</h1>
+                <time dateTime={newsItem.date}>{newsItem.date}</time>
+            </header>
+            <p>{newsItem.content}</p>
+        </article>
     );
 }
